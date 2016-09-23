3616380f8089bc7f0818f02083a062c7d66104c6
Kelly Sexton <Kelly.sexton@interapthq.com>
2016-09-23 08:34:51 -0400

    Periodic Weather App update

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -1,30 +1,40 @@
 $(function() {
 
-var zip =("#textinput")
+var zip = "41824";//add text input here
 
-var googleUrl= "https://maps.googleapis.com/maps/api/geocode/json?address=" + zip + "&key=AIzaSyD_GFnLVIgSuZ1ajEX4BvU3fTERUCC1qbA
-")
+var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zip + "&key=AIzaSyD_GFnLVIgSuZ1ajEX4BvU3fTERUCC1qbA";
 
-var var darkSkyKey = "<your key here>";
+var  darkSkyKey = "b7bc09fbf528eaf0b0cb02eeb7fbf8a4";
 
-var LAT = "";
 
-var LONG = "";
-   
+
+
+  
+  $.ajax(googleUrl)
+    .done(function (data) {
+    $().append(data.results[0].geometry.bounds.northeast.lat);
+
 $.ajax(googleUrl)
     .done(function (data) {
-    $(data.LAT, data.LONG).append(LAT, LONG);
+    $().append(data.results[0].geometry.bounds.northeast.lng);
+
+
+$.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233", { dataType: "jsonp"})
+    .done(function (data) {
+        $("#card1")
+        .append("Temp" + Math.round(data.currently.temperature) + " ")
+        .append("PrecipitationChance" + Math.round(data.currently.precipProbability) + " ")
+        .append("MinTemp" + Math.round(data.daily.data[0].temperatureMin) + " ")
+        .append("MaxTemp" + Math.round(data.daily.data[0].temperatureMax) + " ");
+
+  
+
+
+console.log(data);
 });
-   
-   $.ajax("https://api.darksky.net/forecast/" + b7bc09fbf528eaf0b0cb02eeb7fbf8a4 + LAT + LONG, { dataType: "jsonp"}).done(function(data) {
-       console.log(data);
-   });
-
-
-
 
 });
 
+});
 
-
-
+});
