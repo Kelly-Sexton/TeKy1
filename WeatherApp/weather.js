8ee7f0ac9ba407598fa66c794b3156d971e8f929
Kelly Sexton <Kelly.sexton@interapthq.com>
2016-09-24 15:51:41 -0400

    Add files via upload

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -1,50 +1,24 @@
-$(function() {
-
-// var zip = "41824";//add text input here
-
-$("#zipInputButton").click(function () {
-var zip = $("#zipbox").val();
-
-
-
-
-
-
-
-var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zip + "&key=AIzaSyD_GFnLVIgSuZ1ajEX4BvU3fTERUCC1qbA";
-
-var  darkSkyKey = "b7bc09fbf528eaf0b0cb02eeb7fbf8a4";
-
-
-
-  
-  $.ajax(googleUrl)
-    .done(function (data) {
-    $().append(data.results[0].geometry.bounds.northeast.lat);
-
-$.ajax(googleUrl)
-    .done(function (data) {
-    $().append(data.results[0].geometry.bounds.northeast.lng);
-
-
-$.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233", { dataType: "jsonp"})
-    .done(function (data) {
+$(function () {
+    // var darkSkyKey = "b7bc09fbf528eaf0b0cb02eeb7fbf8a4";
+    var googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+//    $("zipButton").click(function(ev){
+        // var zip = $("#zipBox").val();
+        var zip = "41824";
+        $.ajax(googleUrl + zip + "&key=AIzaSyD_GFnLVIgSuZ1ajEX4BvU3fTERUCC1qbA")
+        .done(function(data){
+        var lat = "" ;
+        var lng = "" ;
+        $(lat).append(data.results[0].geometry.bounds.northeast.lat)
+        $(lng).append(data.results[0].geometry.bounds.northeast.lng)
+        
+    $.ajax("https://api.darksky.net/forecast/b7bc09fbf528eaf0b0cb02eeb7fbf8a4/37.8267,-122.4233",
+    { dataType: "jsonp"})
+    .done(function(data) {
         $("#card1")
         .append("Temp" + Math.round(data.currently.temperature) + " ")
-        .append("PrecipitationChance" + Math.round(data.currently.precipProbability) + " ")
+        .append("PrecipitationChance" + Math.round(data.currently.precipProbility) + " ")
         .append("MinTemp" + Math.round(data.daily.data[0].temperatureMin) + " ")
         .append("MaxTemp" + Math.round(data.daily.data[0].temperatureMax) + " ");
-
-  
-
-
-console.log(data);
-});
-
-});
-
-});
-
-});
-
+        })
+    })
 });
